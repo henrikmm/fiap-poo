695a10e0ac5f3259c03491957b21db6a5b6ae0dd
Henrique Mandrick <henrique_mandrick@hotmail.com>
2026-03-13 17:49:42 -0300

    feat: adiciona métodos de comportamento

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,50 +1,61 @@
 package br.com.fiapride.main;
 
-// Importamos a classe Passageiro para que o sistema a reconheça
 import br.com.fiapride.model.Passageiro;
 import br.com.fiapride.model.Sapato;
-import br.com.fiapride.model.Sapato.TipoSapato;
-
 
 public class SistemaPrincipal {
 
     public static void main(String[] args) {
-        // INSTANCIAÇÃO
-        // O comando 'new' aloca memória para um novo objeto.
-        // Criando o primeiro passageiro (Objeto 1)
-    //  Passageiro passageiro1 = new Passageiro();
-    //  passageiro1.nome = "Ana Silva";
-    //  passageiro1.saldo = 50.0;
+        // PASSAGEIROS
+    	
+    	/*
+        Passageiro passageiro1 = new Passageiro("Ana Silva", "222");
+        System.out.println("Recarga passageiro 1");
+        passageiro1.adicionarSaldo(50.0);
 
-        // Criando o segundo passageiro (Objeto 2)
-    //  Passageiro passageiro2 = new Passageiro();
-    //  passageiro2.nome = "Carlos Souza";
-    //  passageiro2.saldo = 12.50;
+        Passageiro passageiro2 = new Passageiro("Carlos Souza", "333");
+        System.out.println("Recarga passageiro 2");
+        passageiro2.adicionarSaldo(12.5);
+
+        System.out.println("--- Sistema FiapRide ---");
         
-        //  Instaciação
-        // Criando o primeiro sapato (objeto1)
+        System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$ " + passageiro1.saldo + " | CPF: " + passageiro1.cpf);
+        System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$ " + passageiro2.saldo + " | CPF: " + passageiro2.cpf);
+       
+        
+        
+        System.out.println("Pagando viagem do passageiro 1");
+        passageiro1.pagarViagem(20);
+		
+        System.out.println("Pagando viagem do passageiro 2");
+        passageiro2.pagarViagem(20);
+       	*/
+
+        // SAPATOS
         Sapato sapato1 = new Sapato();
-       sapato1.tamanho = 40;
-       sapato1.tipo = Sapato.TipoSapato.TENIS;
-       sapato1.cadarco = true;
-        
-        // objeto 2
-        
-        
+        sapato1.tamanho = 40;
+        sapato1.tipo = Sapato.TipoSapato.TENIS;
+        sapato1.cadarco = true;
+
         Sapato sapato2 = new Sapato();
         sapato2.tamanho = 42;
         sapato2.tipo = Sapato.TipoSapato.SANDALIA;
         sapato2.cadarco = false;
-        
-        
-        
 
-        // Exibindo os dados no Console
-        System.out.println("--- Sistema FiapRide ---");
+        System.out.println("--- Testes dos métodos da classe Sapato ---");
+
+        // teste válido
+        sapato1.alterarTipo(Sapato.TipoSapato.BOTA);
+        sapato1.alterarCadarco(false);
+
+        // teste inválido
+        sapato2.alterarCadarco(true);
+
+        // teste inválido
+        sapato1.alterarTipo(null);
+
+        System.out.println("--- Estado final dos sapatos ---");
         System.out.println("Tipo do sapato: " + sapato1.tipo + " | tamanho: " + sapato1.tamanho + " | possui cadarço: " + sapato1.cadarco);
         System.out.println("Tipo do sapato: " + sapato2.tipo + " | tamanho: " + sapato2.tamanho + " | possui cadarço: " + sapato2.cadarco);
-        
-        
-        // Teste mental: Se eu mudar o nome do passageiro1, o passageiro2 muda?
     }
 }
--- src/br/com/fiapride/model/Sapato.java
+++ src/br/com/fiapride/model/Sapato.java
@@ -15,6 +15,31 @@ public class Sapato { // <-- Use o nome do SEU objeto
     public TipoSapato tipo;
     public boolean  cadarco;
     
+    public void alterarTipo(TipoSapato novoTipo) {
+        if (novoTipo == null) {
+            System.out.println("Erro: o tipo do sapato não pode ser nulo.");
+            return;
+        }
+
+        if (novoTipo == TipoSapato.SANDALIA && this.cadarco == true) {
+            System.out.println("Erro: sandália não pode possuir cadarço.");
+            return;
+        }
+
+        this.tipo = novoTipo;
+        System.out.println("Tipo alterado com sucesso para: " + this.tipo);
+    }
+    
+    public void alterarCadarco(boolean possuiCadarco) {
+        if (this.tipo == TipoSapato.SANDALIA && possuiCadarco == true) {
+            System.out.println("Erro: sandália não pode possuir cadarço.");
+            return;
+        }
+
+        this.cadarco = possuiCadarco;
+        System.out.println("Cadarço alterado com sucesso para: " + this.cadarco);
+    }
+}
+    
    
     
-}
